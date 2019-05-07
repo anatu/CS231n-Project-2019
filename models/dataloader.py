8b3a2c4f4f5468823d0e3e9be5292bd91d0f8d79
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-05-07 00:02:31 +0000

    cleaned up dataloader.py and edited vgg.py to run on gpu

--- models/dataloader.py
+++ models/dataloader.py
@@ -15,14 +15,19 @@ from pathlib import Path
 userPath = os.path.expanduser("~")
 
 
+######## #### #### #### #### #### #### #### #### #### #### #### ########
+### File that handles all data processing / loading                  ###
+### Take the resized images and port into train, val, test folders   ###
+######## #### #### #### #### #### #### #### #### #### #### #### ########
 
-# DATA PROCESSING
-# Take the resized images and port into train, val, test folders
-
-## splits indices for a folder into train, validation, and test indices with random sampling
-    ## input: folder path
-    ## output: train, valid, and test indices    
-def split_indices(folder,seed1,seed2):    
+def split_indices(folder, seed1, seed2):
+    '''
+    Function to split indices for a folder into train, validation, and test indices with random sampling.
+    Takes as input:
+    - folder: folder path
+    - seed1, seed2: seeds for random init
+    Returns  (train, valid, test) indices
+    '''
     n = len(os.listdir(folder))
     full_set = list(range(1,n+1))
 
@@ -37,31 +42,40 @@ def split_indices(folder,seed1,seed2):
     random.seed(seed2)
     valid = random.sample(remain,int(.5*len(remain)))
     test = list(set(remain)-set(valid))
+
     return(train,valid,test)
 
-## gets file names for a particular type of trash, given indices
-    ## input: waste category and indices
-    ## output: file names 
-def get_names(waste_type,indices):
+
+
+def get_names(waste_type, indices):
+    '''
+    Function to get the filenames for a particular type of trash, given indices.
+    - Parameters:  waste_type: the category of waste
+                   indices: indices for the train, valid, and test datasets
+    - Returns:     file names    
+    '''
     file_names = [waste_type+str(i)+".jpg" for i in indices]
     return(file_names)    
 
-## moves group of source files to another folder
-    ## input: list of source files and destination folder
-    ## no output
-def move_files(source_files,destination_folder):
+
+def move_files(source_files, destination_folder):
+    '''
+    Function to move group of source files to another folder.
+    Takes as input the list of source files and destination folder.  
+    '''
     for file in source_files:
         # Copy files so they still live in the source folder in case we need to repeat / change the operation
         # without having to re-downlaod in the source data
-        shutil.copy(file,destination_folder)
+        shutil.copy(file, destination_folder)
 
-
-# DATA PROCESSING
-# Make train, val, and test folders for data (preserving the 
-# child folder structure which tells us the waste type)
-## paths will be train/cardboard, train/glass, etc...
+        
 def split_train_val_test():
-
+    '''
+    Function that creates the train, val, and test folders for data (preserving the 
+    child folder structure which tells us the waste type). 
+    Paths will be train/cardboard, train/glass, etc...  
+    '''
+    
     subsets = ['train','val']
     waste_types = ['cardboard','glass','metal','paper','plastic','trash']
     
@@ -100,9 +114,11 @@ def split_train_val_test():
                         move_files(test_source_files, userPath.replace("\\", "/") + "/" + outBase + "/test")
 
 
-
 def augment():
-    """ Data Augmentation - Loading the dataset into memory and applying transforms """
+    ''' Function that sets up data augmentation transforms.
+    After loading the data into memory, can call this function to get the transforms and apply
+    them to the data.
+    '''
     # Data augmentation and normalization for training
     # Just normalization for validation
     data_transforms = {
@@ -150,5 +166,8 @@ def load_data(outDataPath):
 
 
 if __name__ == "__main__":
-    outpath = "/Users/sarahciresi/Documents/GitHub/CS231n-Project-2019/datasets/trashnet/data"  
+    #outpath = "/Users/sarahciresi/Documents/GitHub/CS231n-Project-2019/datasets/trashnet/data"  
+    outpath = "/home/sarahciresi/gcloud/project/CS231n-Project-2019/datasets/trashnet/data" 
+    
     load_data(outpath)
+    
--- models/vgg.py
+++ models/vgg.py
@@ -12,41 +12,45 @@ from dataloader import load_data
 
 class VGG(object):
 
-    def __init__(self, pretrained_model, num_classes=6, learning_rate=0.0001, weight_scale=1e-3, reg=0.0, dtype=np.float32):
-        """ Initialize a new network based on VGG architecture"""
+    def __init__(self, pretrained_model, device, num_classes=6, learning_rate=0.0001, weight_scale=1e-3, reg=0.0, dtype=np.float32):
+        '''
+        Initialize a new network based on VGG architecture
+        '''
+
         self.params = {}
         self.reg = reg
-        self.dtype = dtype
+        self.dtype = dtype 
         self.model = pretrained_model
         self.num_classes = num_classes
         self.lr = learning_rate
         self.weight_scale = weight_scale
-
-        # Freeze all layers 
+        self.device = device
+        self.save_model_path = "./best_model_weights.pt"
+        
+        # Freeze all original layers 
         for param in self.model.features.parameters():
             param.require_grad = False
         
-        # Newly created modules have require_grad=True by default
+        # Remove last fully connected layer and replace with layer with 6 output classes
         num_features = self.model.classifier[6].in_features
-        features = list(self.model.classifier.children())[:-1] # Remove last layer
-        features.extend([nn.Linear(num_features, num_classes)]) # Add final linear layer
+        features = list(self.model.classifier.children())[:-1]                  # Remove last layer
+        features.extend([nn.Linear(num_features, num_classes).to(self.device)]) # Add final linear layer
 
-        self.model.classifier = nn.Sequential(*features) # Replace the model classifier
-        #print(self.model)
+        self.model.classifier = nn.Sequential(*features) #.to(self.device)   # Replace the model classifier
+
         
-    def loss(self, X, y=None):
-        """ Evaluate loss and gradient for the network """
-        pass
 
 
     def train(self, dataloaders, dataset_sizes, num_epochs = 25):
-
+        ''' Function to train the model. Takes as input:
+        - dataloaders:     dataloaders for the train and validation datasets
+        - dataset_sizes:   sizes of the train and validation datasets    
+        - num_epochs:      number of epochs to train for 
+        '''
+        
         best_model_wts = copy.deepcopy(self.model.state_dict())
         best_acc = 0.0
 
-        #train_batches = len(dataloaders["train"])
-        #val_batches = len(dataloaders["val"])
-    
         loss_fn = nn.CrossEntropyLoss()
         optimizer = optim.Adam(self.model.parameters(), lr = self.lr)
 
@@ -65,8 +69,8 @@ class VGG(object):
                 total_correct = 0 
 
                 for inputs, labels in dataloaders[mode]:
-                    inputs = inputs.to(device)
-                    labels = labels.to(device)
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
                     
                     # Zero out the gradient for this next pass
                     optimizer.zero_grad()
@@ -103,34 +107,58 @@ class VGG(object):
 
         print('Best val Acc: {:4f}'.format(best_acc))
 
-        # load best model weights
+        # Load best model weights
         self.model.load_state_dict(best_model_wts)
+
+        # Save best model weights to disk for later loading
+        torch.save(self.model.state_dict(), self.save_model_path)
+        
         return self.model
 
+    
+    def load_model(self, path, train_mode = False):
+        ''' Function to load the model weights from specified path. 
+        If train_mode is set to true, model will be trained after loading the weights.
+        Otherwise, model will just be used for evaluation, and model must be set to eval mode.
+        '''
 
+        self.model.load_state_dict(torch.load(path))
+        self.model.to(self.device)
 
+        # Remember to call self.model.eval() if only evaluating with this reloaded model
+        if train_mode == False:
+            self.model.eval()
+
+        return self.model
+
+    
 if __name__ == "__main__":
     
     # Load the data
-    pathname = "/Users/sarahciresi/Documents/GitHub/CS231n-Project-2019/datasets/trashnet/data"
+    #pathname = "/Users/sarahciresi/Documents/GitHub/CS231n-Project-2019/datasets/trashnet/data"
+    pathname = "/home/sarahciresi/gcloud/project/CS231n-Project-2019/datasets/trashnet/data"
+         
     dataloaders, dataset_sizes, class_names = load_data(pathname)
 
+    # unsure if have to change input data to type dtype=torch.cuda.FloatTensor
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     vgg16 = models.vgg16(pretrained=True).to(device)
-
+    
     # Initialize the model
-    model = VGG(vgg16)
+    vgg_model = VGG(vgg16, device)
 
     # Train the last layer of the model
-    model = model.train(dataloaders, dataset_sizes, num_epochs=25)
+    vgg_model = vgg_model.train(dataloaders, dataset_sizes, num_epochs=5)
 
+    # Or load from saved weights from previously retrained model
+    # vgg_model = vgg_model.load_model("./best_model_weights.pt", train_mode = False)
     
 
 
     
 
 '''
-### models ###
+### Other models ###
 resnet18 = models.resnet18(pretrained=True)
 alexnet = models.alexnet(pretrained=True)
 squeezenet = models.squeezenet1_0(pretrained=True)
