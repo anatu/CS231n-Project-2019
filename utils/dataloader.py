f42ab6ff354ebd9f52a7fdd8da3d4a147c9bf964
Sarah <sciresi@stanford.edu>
2019-06-04 14:21:32 +0000

    added photos to the train/val/test splits and updated dataloader.py to load the test dataset

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -133,21 +133,30 @@ def augment():
     them to the data.
     '''
     # Data augmentation and normalization for training
-    # Just normalization for validation
+    # Just normalization for validation and test sets
     data_transforms = {
         'train': transforms.Compose([
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-                ]),
+                transforms.Normalize([0.5417, 0.5311, 0.5700], [0.7856, 0.7939, 0.8158])
+                #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])            
+        ]),
         'val': transforms.Compose([
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                transforms.Normalize([0.5417, 0.5311, 0.5700], [0.7856, 0.7939, 0.8158])
+                #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                 ]),
-        }
+        'test': transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5417, 0.5311, 0.5700], [0.7856, 0.7939, 0.8158])
+                #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
     return data_transforms
 
 
@@ -163,19 +172,45 @@ def load_data(outDataPath):
     # Create an ImageFolder dataloader for the input data
     # See https://pytorch.org/docs/stable/torchvision/datasets.html#imagefolder
     image_datasets = {x: datasets.ImageFolder(os.path.join(outDataPath, x),
-                                              data_transforms[x]) for x in ["train", "val"]}
+                                              data_transforms[x]) for x in ["train", "val", "test"]}
 
+        
     # Create DataLoader objects for each of the image datasets returned by ImageFolder
     # See https://pytorch.org/docs/stable/data.html
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=64,
-                                                  shuffle=True, num_workers=4) for x in ['train', 'val']}
+                                                  shuffle=True, num_workers=4) for x in ['train', 'val', 'test']}
     
-    
-    datasets_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-    
+    datasets_sizes = {x: len(image_datasets[x]) for x in ['train', 'val', 'test']}
     class_names = image_datasets['train'].classes
     
     return dataloaders, datasets_sizes, class_names
+
+
+
+def compute_mean_var(dataloaders, dataset = 'train'):    
+    ''' Computes the mean and std of the images in the training dataset
+    to be used for normalizing the train, val, and test sets'''
+    
+    loader = dataloaders[dataset]
+    
+    mean = 0.0
+    for images, _ in loader:
+        batch_samples = images.size(0)
+        images = images.view(batch_samples, images.size(1), -1)
+        mean += images.mean(2).sum(0)
+    mean = mean / len(loader.dataset)
+        
+    var = 0.0
+    for images, _ in loader:
+        batch_samples = images.size(0)
+        images = images.view(batch_samples, images.size(1), -1)
+        var += ((images - mean.unsqueeze(1))**2).sum([0,2])
+    std = torch.sqrt(var / (len(loader.dataset)*224*224))
+
+    return mean, std
+
+
+
 
 
 if __name__ == "__main__":
@@ -183,10 +218,9 @@ if __name__ == "__main__":
     ## If this is the file being run as the main method, it creates the train/val/test split folders ##
     split_train_val_test()
     
+    ## Otherwise it is called from the model files (soon to be experiments) to load the data into
+    # dataloader objects and train and evaluate the models
 
-    ## otherwise use from the vgg.py file to load the data into dataloader objects ##
-    #outpath = "/Users/sarahciresi/Documents/GitHub/CS231n-Project-2019/datasets/trashnet/data"  
-    #outpath = "/home/sarahciresi/gcloud/project/CS231n-Project-2019/datasets/trashnet/data" 
     
     
     
